102df60d63fde66f20aa95c4437f4120f8db6931
Abhishek Rawat <131130116+Abhirawat623@users.noreply.github.com>
2024-02-10 23:02:27 +0530

    login structure made

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import {LandingPage, SignInPage, SignUpPage,  ForgetPasswordPage, HomePage,} from './components/pages/index' 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+export default function App() {
+    return (
+       
+            <div>
+                <Routes>
+                    <Route exact path="/" element={ <LandingPage/> } />
+                    <Route path="/login" element={ <SignInPage/> } />
+                    <Route path="/register" element={ <SignUpPage/> } />
+                    <Route path="/forget-password" element={ <ForgetPasswordPage/> } />
+                    <Route path="/home" element={ <HomePage/> } />
+                </Routes>
+                <Footer />
+            </div>
+      
+    )
 }
 
-export default App;
+const Footer = () => {
+    return (
+        <p className="text-center" style={ FooterStyle }>Designed & coded by<a className='author-text' href="https://my-portfolio-gi7r.vercel.app/" target="_blank" rel="noopener noreferrer">
+               Abhishek Rawat</a></p>
+    )
+}
+const FooterStyle = {
+    background: "#222",
+    fontSize: ".8rem",
+    color: "#fff",
+    position: "absolute",
+    bottom: 0,
+    padding: "1rem",
+    margin: 0,
+    width: "100%",
+    opacity: ".5"
+}
---
+++ src/components/pages/index.js
@@ -0,0 +1,5 @@
+export {ForgetPasswordPage} from '../pages/ForgetPasswordPage';
+export {HomePage} from '../pages/HomePage';
+export {LandingPage} from '../pages/LandingPage';
+export {SignInPage} from '../pages/LoginPage';
+export {SignUpPage} from './RegisterPage';
